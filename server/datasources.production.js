bab27b47687eaeed984d132a800725f0c26761f7
Ramiru <blackhawk111@yandex.ru>
2018-10-30 00:30:48 +0300

    change db

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -4,7 +4,6 @@ module.exports = {
   "db": {
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
-    "url": process.env.DB_USER,
     "database": "rest_test",
     "password": process.env.DB_PASSWORD,
     "name": "mongoDS",
