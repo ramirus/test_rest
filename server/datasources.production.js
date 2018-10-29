04802bad563350b3cdb898190067cbd249a0a940
Ramiru <blackhawk111@yandex.ru>
2018-10-30 00:26:22 +0300

    1

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,8 +1,14 @@
 'use strict';
 
 module.exports = {
-  db: {
-    url: process.env.MONGODB_URI,
-    connector: "mongodb"
+  "db": {
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "url": process.env.DB_USER,
+    "database": "rest_test",
+    "password": process.env.DB_PASSWORD,
+    "name": "mongoDS",
+    "user": process.env.DB_USER,
+    "connector": "mongodb"
   }
 };
