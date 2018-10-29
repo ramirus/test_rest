20e7587f3c0deead987c9436685da46396d083fd
Ramiru <blackhawk111@yandex.ru>
2018-10-30 00:35:36 +0300

    fix

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -5,6 +5,7 @@ module.exports = {
     "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "database": "rest_test",
+    "url": process.env.MONGODB_URI,
     "password": process.env.DB_PASSWORD,
     "name": "mongoDS",
     "user": process.env.DB_USER,
